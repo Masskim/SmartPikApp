716dcb27b22e2173a8e80dd6066e6e5dda72021f
Origami0067 <volpatogerard@yahoo.com>
2021-10-19 08:16:36 +0200

    access camera

--- app/src/main/java/com/smartpik/app/MainActivity.java
+++ app/src/main/java/com/smartpik/app/MainActivity.java
@@ -1,18 +1,28 @@
 package com.smartpik.app;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraX;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureConfig;
+import androidx.camera.core.Preview;
+import androidx.camera.core.PreviewConfig;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
-
 import android.content.pm.PackageManager;
+import android.opengl.Matrix;
 import android.os.Bundle;
+import android.util.Rational;
+import android.util.Size;
+import android.view.Surface;
 import android.view.TextureView;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.io.File;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,6 +48,92 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startCamera() {
+        CameraX.unbindAll();
+
+        Rational aspectRatio = new Rational(textureView.getWidth(),textureView.getHeight());
+        Size screen = new Size(textureView.getWidth(),textureView.getHeight());
+
+        PreviewConfig pConfig = new PreviewConfig.Builder().setTargetAspectRatio(aspectRatio).setTargetResolution(screen).build();
+        Preview preview = new Preview(pConfig);
+
+        preview.setOnPreviewOutputUpdateListener(
+                new Preview.OnPreviewOutputUpdateListener() {
+                    @Override
+                    public void onUpdated(Preview.PreviewOutput output) {
+                        ViewGroup parent = (ViewGroup) textureView.getParent();
+                        parent.removeView(textureView);
+                        parent.addView(textureView);
+
+                        textureView.setSurfaceTexture(output.getSurfaceTexture());
+                        updateTransform();
+
+                    }
+
+                }
+        );
+
+        ImageCaptureConfig imageCaptureConfig = new ImageCaptureConfig.Builder().setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY).
+                setTargetRotation(getWindowManager().getDefaultDisplay().getRotation()).build();
+
+        final ImageCapture imgCap = new ImageCapture(imageCaptureConfig);
+
+        findViewById(R.id.imgCapture).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                File file = new File("sdcard/photo/DCIM(0)/Camera/CameraX" + System.currentTimeMillis());
+                imgCap.takePicture(file, new ImageCapture.OnImageSavedListener() {
+                    @Override
+                    public void onImageSaved(@NonNull File file) {
+                        String msg = "pic captured at "+file.getAbsolutePath();
+                        Toast.makeText(getBaseContext(),msg,Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCapture.UseCaseError useCaseError, @NonNull String message, @Nullable Throwable cause) {
+                        String msg = "pic capture failed:" + message;
+                        Toast.makeText(getBaseContext(),msg,Toast.LENGTH_LONG).show();
+
+                        if(cause != null){
+                            cause.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+
+        CameraX.bindToLifecycle(this,preview,imgCap);
+    }
+
+    private void updateTransform() {
+        Matrix mx = new Matrix();
+        float w = textureView.getMeasuredWidth();
+        float h = textureView.getMeasuredHeight();
+
+        float cx = w/2f;
+        float cy = h/2f;
+
+        int rotationDgr;
+        int rotation = (int)textureView.getRotation();
+
+        switch(rotation) {
+            case Surface.ROTATION_0:
+                rotationDgr = 0;
+                break;
+            case Surface.ROTATION_90:
+                rotationDgr = 90;
+                break;
+            case Surface.ROTATION_180:
+                rotationDgr = 180;
+                break;
+            case Surface.ROTATION_270:
+                rotationDgr = 270;
+                break;
+            default:
+                return;
+        }
+
+        //mx.postRotate((float)rotationDgr,cx,cy);
     }
 
     private boolean allPermissionGranted() {
