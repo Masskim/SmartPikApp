9d2ba1e9c38a2bf3181af2d7f28065deb953b430
Masskim <maxime.perger.etu@univ-lemans.fr>
2022-01-24 15:48:26 +0100

    Base

--- app/src/main/java/com/smartpik/app/MainActivity.java
+++ app/src/main/java/com/smartpik/app/MainActivity.java
@@ -1,155 +1,157 @@
 package com.smartpik.app;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.CameraX;
-import androidx.camera.core.ImageCapture;
-import androidx.camera.core.ImageCaptureConfig;
-import androidx.camera.core.Preview;
-import androidx.camera.core.PreviewConfig;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+
 import android.content.pm.PackageManager;
-import android.opengl.Matrix;
 import android.os.Bundle;
-import android.os.Environment;
-import android.util.Rational;
-import android.util.Size;
-import android.view.Surface;
-import android.view.TextureView;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-import java.io.File;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.Text;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback, Detector.Processor {
+    private SurfaceView cameraView;
+    private TextView txtView;
+    private CameraSource cameraSource;
+    private ImageButton scanButton;
 
-    private int REQUEST_CODE_PERMISSION = 101;
-    private String[] REQUIRED_PERMISSION = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE"};
-        //test
-    TextureView textureView;
-    public static String photographStorage =
-            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).toString()
-                    + File.separator + "SmartPik";
-    File file = new File(photographStorage);
 
+    @SuppressLint({"MissingPermission", "MissingSuperCall"})
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    try {
+                        cameraSource.start(cameraView.getHolder());
+                    } catch (Exception e) {
 
-        getSupportActionBar().hide();
-        textureView = (TextureView) findViewById(R.id.view_finder);
-
-        if (allPermissionGranted()) {
-            startCamera();
-        }
-        else{
-            ActivityCompat.requestPermissions(this,REQUIRED_PERMISSION,REQUEST_CODE_PERMISSION);
+                    }
+                }
+            }
+            break;
         }
     }
 
-    private void startCamera() {
-        CameraX.unbindAll();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        cameraView = findViewById(R.id.surface_view);
+        txtView = findViewById(R.id.txtview);
+        scanButton = (ImageButton)findViewById(R.id.scanner);
 
-        Rational aspectRatio = new Rational(textureView.getWidth(),textureView.getHeight());
-        Size screen = new Size(textureView.getWidth(),textureView.getHeight());
 
-        PreviewConfig pConfig = new PreviewConfig.Builder().setTargetAspectRatio(aspectRatio).setTargetResolution(screen).build();
-        Preview preview = new Preview(pConfig);
 
-        preview.setOnPreviewOutputUpdateListener(
-                new Preview.OnPreviewOutputUpdateListener() {
-                    @Override
-                    public void onUpdated(Preview.PreviewOutput output) {
-                        ViewGroup parent = (ViewGroup) textureView.getParent();
-                        parent.removeView(textureView);
-                        parent.addView(textureView);
+        TextRecognizer txtRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+        if (!txtRecognizer.isOperational()) {
+            Log.e("Main Activity", "Detector dependencies are not yet available");
+        } else {
+            cameraSource = new CameraSource.Builder(getApplicationContext(), txtRecognizer)
+                    .setFacing(CameraSource.CAMERA_FACING_BACK)
+                    .setRequestedPreviewSize(1280, 1024)
+                    .setRequestedFps(2.0f)
+                    .setAutoFocusEnabled(true)
+                    .build();
+            cameraView.getHolder().addCallback(this);
+            txtRecognizer.setProcessor(this);
+        }
+    }
 
-                        textureView.setSurfaceTexture(output.getSurfaceTexture());
-                        updateTransform();
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},1);
+                return;
+            }
+            cameraSource.start(cameraView.getHolder());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-                    }
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 
-                }
-        );
+    }
 
-        ImageCaptureConfig imageCaptureConfig = new ImageCaptureConfig.Builder().setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY).
-                setTargetRotation(getWindowManager().getDefaultDisplay().getRotation()).build();
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        cameraSource.stop();
+    }
 
-        final ImageCapture imgCap = new ImageCapture(imageCaptureConfig);
+    @Override
+    public void release() {
 
-        findViewById(R.id.imgCapture).setOnClickListener(new View.OnClickListener() {
+    }
+
+    @Override
+    public void receiveDetections(Detector.Detections detections) {
+
+        SparseArray items = detections.getDetectedItems();
+        final StringBuilder strBuilder = new StringBuilder();
+
+        scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
 
-                File file = new File(photographStorage+ File.separator + "IMG_" + System.currentTimeMillis()+".jpg");
-                imgCap.takePicture(file, new ImageCapture.OnImageSavedListener() {
-                    @Override
-                    public void onImageSaved(@NonNull File file) {
-                        String msg = "pic captured at "+file.getAbsolutePath();
-                        Toast.makeText(getBaseContext(),msg,Toast.LENGTH_LONG).show();
+            }
+        });
+        for (int i = 0; i < items.size(); i++)
+        {
+            TextBlock item = (TextBlock)items.valueAt(i);
+            strBuilder.append(item.getValue());
+            strBuilder.append("/");
+            // The following Process is used to show how to use lines & elements as well
+            for (int j = 0; j < items.size(); j++) {
+                TextBlock textBlock = (TextBlock) items.valueAt(j);
+                strBuilder.append(textBlock.getValue());
+                strBuilder.append("/");
+                for (Text line : textBlock.getComponents()) {
+                    //extract scanned text lines here
+                    Log.v("lines", line.getValue());
+                    strBuilder.append(line.getValue());
+                    strBuilder.append("/");
+                    for (Text element : line.getComponents()) {
+                        //extract scanned text words here
+                        Log.v("element", element.getValue());
+                        strBuilder.append(element.getValue());
                     }
+                }
+            }
+        }
+        Log.v("strBuilder.toString()", strBuilder.toString());
 
+        txtView.post(new Runnable() {
+            @Override
+            public void run() {
+                scanButton.setOnClickListener(new View.OnClickListener() {
                     @Override
-                    public void onError(@NonNull ImageCapture.UseCaseError useCaseError, @NonNull String message, @Nullable Throwable cause) {
-                        String msg = "pic capture failed:" + message;
-                        Toast.makeText(getBaseContext(),msg,Toast.LENGTH_LONG).show();
-
-                        if(cause != null){
-                            cause.printStackTrace();
-                        }
+                    public void onClick(View v) {
+                        txtView.setText(strBuilder.toString());
                     }
                 });
+
             }
         });
 
-        CameraX.bindToLifecycle(this,preview,imgCap);
     }
-
-    private void updateTransform() {
-        Matrix mx = new Matrix();
-        float w = textureView.getMeasuredWidth();
-        float h = textureView.getMeasuredHeight();
-
-        float cx = w/2f;
-        float cy = h/2f;
-
-        int rotationDgr;
-        int rotation = (int)textureView.getRotation();
-
-        switch(rotation) {
-            case Surface.ROTATION_0:
-                rotationDgr = 0;
-                break;
-            case Surface.ROTATION_90:
-                rotationDgr = 90;
-                break;
-            case Surface.ROTATION_180:
-                rotationDgr = 180;
-                break;
-            case Surface.ROTATION_270:
-                rotationDgr = 270;
-                break;
-            default:
-                return;
-        }
-
-        //mx.postRotate((float)rotationDgr,cx,cy);
-    }
-
-    private boolean allPermissionGranted() {
-        for(String permission: REQUIRED_PERMISSION){
-            if(ContextCompat.checkSelfPermission(this,permission) != PackageManager.PERMISSION_GRANTED){
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-
 }
